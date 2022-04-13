68101be7e8d1d7e4f64d864cdb4c115f595120b0
Felipe Pardim <felipesantos.2017@alunos.utfpr.edu.br>
2022-04-12 22:16:55 -0300

    Fixing admin boolean atribuiton to user model

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 class User {
   id: string = uuidv4();
   name: string;
-  admin: false;
+  admin: boolean;
   email: string;
   created_at: Date;
   updated_at: Date;
