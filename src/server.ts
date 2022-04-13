1e4354cd58e216b8adf6eee3d9dca4e8e1c8b5c2
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2022-04-13 17:17:37 -0300

    Adding server port value

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
 import { app } from ".";
 
-app.listen(3333, () => console.log("Server is running!"));
+const port = 3333;
+
+app.listen(port, () => console.log(`Server is running!\nPort: ${port}`));
