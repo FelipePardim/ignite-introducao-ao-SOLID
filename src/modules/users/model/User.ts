828e397471ab4f4705ebaad95ab751a87d91e6d1
Felipe Pardim <felipesantos.2017@alunos.utfpr.edu.br>
2022-04-16 18:56:09 -0300

    Fixing user model by implementing constructor for admin and id

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -3,10 +3,17 @@ import { v4 as uuidv4 } from "uuid";
 class User {
   name: string;
   email: string;
-  admin: false | boolean;
-  id: string = uuidv4();
+  admin: boolean;
+  id: string;
   created_at: Date;
   updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+      this.admin = false;
+    }
+  }
 }
 
 export { User };
