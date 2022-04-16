9b2bbb55d336288529c8149e9d6cc772f25e2e16
Felipe Pardim <felipesantos.2017@alunos.utfpr.edu.br>
2022-04-16 19:05:38 -0300

    Adding string type to user id

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,10 +6,12 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.body;
+    const { user_id } = request.headers;
 
     try {
-      const users = this.listAllUsersUseCase.execute(user_id);
+      const users = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
       return response.status(200).send(users);
     } catch (error) {
       return response.status(400).send({ error: error.message });
